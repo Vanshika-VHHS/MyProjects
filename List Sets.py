25d1fa808b66ca022440f185ccc55bdf8f3b0723
Vanshika-VHHS <windvansh@gmail.com>
2026-03-11 18:26:21 -0500

    Add files via upload
    Sorting numbers using List and Set

---
+++ List Sets.py
@@ -0,0 +1,7 @@
+#Using list and set to display unique numbers from a list of values.
+list_values = [1, 2, 3, 7, 1, 8, 2, 9, 4, 5]
+list_values.append(20)
+print (list_values)
+print ("These are the display numbers: ", list_values)
+set_values = set(list_values)
+print (set_values)
